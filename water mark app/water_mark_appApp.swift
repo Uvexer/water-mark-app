6a27c220627740823524d53f8bc9c12faab3487a
LowLife <125573358+Bogdan52003@users.noreply.github.com>
2024-05-20 17:25:43 +0300

    Initial Commit

---
+++ water mark app/water_mark_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_mark_appApp.swift
+//  water mark app
+//
+//  Created by Bogdan Chupakhin on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct water_mark_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
