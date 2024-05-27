6a2cab37ff060f6d1bfaf9507dd743d82bc8f30b
LowLife <125573358+Bogdan52003@users.noreply.github.com>
2024-05-27 22:08:44 +0300

    design buttons  and fix bug save

--- water mark app/ContentView.swift
+++ water mark app/ContentView.swift
@@ -9,34 +9,73 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
+            HStack {
+                Text("Водяные Знаки 💧")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                Spacer()
+            }
+
+            Spacer()
+
             if let inputImage = inputImage {
                 Image(uiImage: inputImage)
                     .resizable()
                     .scaledToFit()
-                    .overlay(WaterMarkView(waterMarkText: waterMarkText, image: inputImage))
+                    .overlay(Text(waterMarkText)
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .background(Color.black.opacity(0.5))
+                                .cornerRadius(5)
+                                .padding(), alignment: .bottom)
             } else {
                 Text("Tap to select a photo")
                     .foregroundColor(.secondary)
             }
 
-            HStack {
-                Button("Load Photo") {
-                    showingImagePicker = true
-                }
-                TextField("Enter watermark text", text: $waterMarkText)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                Button("Save Photo") {
-                    guard let inputImage = inputImage else { return }
-                    let watermarkedImage = WaterMarkView(waterMarkText: waterMarkText, image: inputImage).snapshot()
-                    UIImageWriteToSavedPhotosAlbum(watermarkedImage, nil, nil, nil)
-                    showSavedAlert = true
-                }
-            }.padding()
-
             Spacer()
+
+            VStack {
+                HStack {
+                    Button(action: {
+                        showingImagePicker = true
+                    }) {
+                        Text("Load Photo")
+                            .font(.headline)
+                            .foregroundColor(.white.opacity(0.5))
+                            .padding()
+                            .background(Color.green.opacity(0.5))
+                            .cornerRadius(10)
+                    }
+
+                    TextField("Enter watermark text", text: $waterMarkText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+
+                    Button(action: {
+                        guard let inputImage = inputImage else { return }
+                        let watermarkedImage = addWatermark(image: inputImage, watermark: waterMarkText)
+                        if let compressedImage = compressImage(image: watermarkedImage, maxFileSize: 30 * 1024 * 1024) {
+                            UIImageWriteToSavedPhotosAlbum(compressedImage, nil, nil, nil)
+                            showSavedAlert = true
+                        }
+                    }) {
+                        Text("Save Photo")
+                            .font(.headline)
+                            .foregroundColor(.white.opacity(0.5))
+                            .padding()
+                            .background(Color.blue.opacity(0.5))
+                            .cornerRadius(10)
+                    }
+                }
+                .padding()
+            }
+
         }
         .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-          PhotoPicker(image: $inputImage)
+            PhotoPicker(image: $inputImage)
         }
         .alert(isPresented: $showSavedAlert) {
             Alert(title: Text("Saved"), message: Text("Your image has been saved to your photos."), dismissButton: .default(Text("OK")))
@@ -46,37 +85,43 @@ struct ContentView: View {
     func loadImage() {
         // This function would handle any post-processing after picking the image if needed
     }
-}
 
-struct WaterMarkView: View {
-    var waterMarkText: String
-    var image: UIImage
+    func compressImage(image: UIImage, maxFileSize: Int) -> UIImage? {
+        var compression: CGFloat = 1.0
+        guard var imageData = image.jpegData(compressionQuality: compression) else { return nil }
 
-    var body: some View {
-        Image(uiImage: image)
-            .resizable()
-            .scaledToFit()
-            .overlay(
-                Text(waterMarkText)
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding(10)
-                    .background(Color.black.opacity(0.5))
-                    .cornerRadius(5),
-                alignment: .center
-            )
+        while imageData.count > maxFileSize && compression > 0 {
+            compression -= 0.1
+            guard let newImageData = image.jpegData(compressionQuality: compression) else { break }
+            imageData = newImageData
+        }
+
+        return UIImage(data: imageData)
     }
 
-    func snapshot() -> UIImage {
-        let controller = UIHostingController(rootView: self)
-        let view = controller.view
-        let targetSize = controller.view.intrinsicContentSize
-        view?.bounds = CGRect(origin: .zero, size: targetSize)
-        view?.backgroundColor = .clear
+    func addWatermark(image: UIImage, watermark: String) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: image.size)
+        return renderer.image { context in
+            image.draw(at: CGPoint.zero)
 
-        let renderer = UIGraphicsImageRenderer(size: targetSize)
-        return renderer.image { _ in
-            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 100),
+                .foregroundColor: UIColor.white.withAlphaComponent(0.5),
+                .backgroundColor: UIColor.black.withAlphaComponent(0.5),
+                .paragraphStyle: paragraphStyle
+            ]
+
+            let string = NSString(string: watermark)
+            let textSize = string.size(withAttributes: attrs)
+            let textRect = CGRect(x: (image.size.width - textSize.width) / 2,
+                                  y: image.size.height - textSize.height - 20,
+                                  width: textSize.width,
+                                  height: textSize.height)
+
+            string.draw(with: textRect, options: .usesLineFragmentOrigin, attributes: attrs, context: nil)
         }
     }
 }
@@ -121,10 +166,4 @@ struct PhotoPicker: UIViewControllerRepresentable {
             }
         }
     }
-}
-
-
-
-#Preview(){
-    ContentView()
 }
